0f4083532afa03f35812ddf667cb1932031f986f
ElegantPencil47 <kazuki.komi538@gmail.com>
2025-09-24 17:55:02 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from flask import jsonify
 # -------------------------------
 app = Flask(__name__)
 app.secret_key = 'your_super_secret_key_here'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 DATABASE = "bbs.db"
 last_posts = {}
@@ -234,6 +235,9 @@ def posts_json(thread_id):
 # -------------------------------
 # アプリ起動
 # -------------------------------
+import os
+
 if __name__ == "__main__":
     init_db()
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
