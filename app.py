1583be1729aa9803533d29cb86d1a391f447265f
ElegantPencil47 <kazuki.komi538@gmail.com>
2025-10-02 18:38:45 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from flask import request
 # -------------------------------
 app = Flask(__name__)
 app.secret_key = 'your_super_secret_key_here'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
 
 
 
