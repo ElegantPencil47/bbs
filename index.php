a3862826fe5e26288fd7dfc9c6d177ac92587935
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-06-26 14:25:37 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $name = $_POST['name'];
   $comment = $_POST['comment'];
   $time = date('Y-m-d H:i:s');
-  $post = 'TEST<a href="' . $comment . '.php"' . '>TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<hr style="border: 2px solid white;">';
+  $post = 'TEST<a href="' . $comment . '.php"' . '>TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<p class="hai">@user_testTEST</p>TEST<hr style="border: 2px solid #A9B2C3;">';
 
   file_put_contents('posts.txt', $post, FILE_APPEND);
   $title = $_POST['comment'] . '.php';
@@ -20,6 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <title>LunarEclipse</title>
 </head>
     <style>
+        .hai{
+          color: #A9B2C3;
+        }
          
         a {
           color: #fff;;
@@ -78,6 +81,8 @@ $posts = str_replace("TEST&lt;/a","</a",$posts);
 $posts = str_replace("TEST&lt;h2","<h2",$posts);
 $posts = str_replace("TEST&lt;hr","<hr",$posts);
 $posts = str_replace("TEST&lt;/h2","</h2",$posts);
+$posts = str_replace("TEST&lt;/p","</p",$posts);
+$posts = str_replace("TEST&lt;p","<p",$posts);
 $posts = str_replace("&gt;",">",$posts);
 $posts = str_replace("&quot;","\"",$posts);
 ?>
