cb28cf4896ae015190c04f034f2339fec1438711
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-08-31 10:17:24 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ echo $alert2;
   $title = date('YmdHis') . '.php';
   
   $comment = $_POST['comment'];
-  $log = $comment . $name
+  $log = $comment . $name;
   $time = date('Y-m-d H:i:s');
   $post = 'TEST<div style="display: flex; align-items: baseline; gap: 10px;">TEST<a href="' . $title . '">TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<p class="hai">' . $name . 'TEST</p>TEST</div>TEST<hr style="border: 2px solid #A9B2C3;">' . \n;
 
