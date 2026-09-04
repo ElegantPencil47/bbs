bb617b3e486bcd1c5e7571bcc553b1a508432529
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-09-04 10:35:11 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -420,7 +420,7 @@ header('Location: ' . $_SERVER['REQUEST_URI']);
 <p id="warning"></p>
 <?php
 
-$error = "";
+$error = array();
  if ($error['re_captha'] === 'failed'):?>※認証に失敗しました。<?php endif; ?>
 <div class="wrap_btn">
   <button id="send" class="btn_st arrow bg_yellow">確認する</button>
