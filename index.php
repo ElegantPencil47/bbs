56a0f8169eca6abdf7b921209ec78876322eacf3
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-09-10 10:05:01 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -92,6 +92,7 @@ echo $alert3;
   $log = $comment . $name . "\n";
   $time = date('Y-m-d H:i:s');
   $post = 'TEST<div style="display: flex; align-items: baseline; gap: 10px;">TEST<a href="' . $title . '">TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<p class="hai">' . $name . 'TEST</p>TEST</div>TEST<hr style="border: 2px solid #A9B2C3;">' . "\n";
+ $post = $post . "TEST<br>" . 'TEST<i class="fa-solid fa-thumbs-up">TEST</i>' . 'TEST<i class="fa-solid fa-download">TEST</i>' . 'TEST<i class="fa-solid fa-arrow-up-from-bracket">TEST</i>';
   if (!file_exists('posts.txt')) {
   file_put_contents('posts.txt', pack("C*", 0xEF, 0xBB, 0xBF));
   }
@@ -108,7 +109,7 @@ echo $alert3;
   file_put_contents('log.txt', $log, FILE_APPEND);
   file_put_contents($jikan . '.txt', "\n" . 'TEST<h1 class="neon_blue">' . $comment . 'TEST</h1>' . 'TEST<h3 class = "neon">' . $name . 'TEST</h3>TEST<p class="green_neon">' . $time . 'TEST</p>', FILE_APPEND);
   
- $post = $post . "TEST<br>" . 'TEST<i class="fa-solid fa-thumbs-up">TEST</i>' . 'TEST<i class="fa-solid fa-download">TEST</i>' . 'TEST<i class="fa-solid fa-arrow-up-from-bracket">TEST</i>';
+
   if($_POST['comment'] == ""){
 header('Location: ' . $_SERVER['REQUEST_URI']);
   exit;
