bf8c9de036d81445145e0674662d21fb34923259
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-09-10 09:26:13 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -201,6 +201,11 @@ header('Location: ' . $_SERVER['REQUEST_URI']);
             0 0 80px #05ff05,
             0 0 100px #05ff05;
         }
+        .sns{
+
+font-size:39px; 
+
+}
 
         .nico{
   position: relative;
@@ -548,10 +553,10 @@ file_put_contents('posts.txt','');
 
 
 
-<div class="bana"><br><a href="https://lunareclipse.onrender.com"><p>トップに戻る</p></a><a href="https://lunareclipse.onrender.com/admin.html"><p>管理人のプロフィール</p></a><a href=""><p>お問い合わせ</p></a><a href=""><p>LunarEclipseについて</p></a><div class="sns">
-<a href = "https://x.com/ElegantPencil47"><i class="fa-brands fa-square-x-twitter" width="18" height="19"></i></a>
-<a href = ""><i class="fa-brands fa-square-instagram" width="18" height="19"></i></a>
-<a href = "https://www.youtube.com/@%E6%A2%85%E3%81%AE%E3%83%AC%E3%83%A2%E3%83%B3%E6%BC%AC%E3%81%91"><i class="fa-brands fa-square-youtube" width="18" height="19"></i></a>
+<div class="bana"><br><a href="https://lunareclipse.onrender.com"><p>トップに戻る</p></a><a href="https://lunareclipse.onrender.com/admin.html"><p>管理人のプロフィール</p></a><a href=""><p>お問い合わせ</p></a><a href=""><p>LunarEclipseについて</p></a><div>
+<a href = "https://x.com/ElegantPencil47" class="sns"><i class="fa-brands fa-square-x-twitter" width="18" height="19"></i></a>
+<a href = "" class="sns"><i class="fa-brands fa-square-instagram" width="18" height="19"></i></a>
+<a href = "https://www.youtube.com/@%E6%A2%85%E3%81%AE%E3%83%AC%E3%83%A2%E3%83%B3%E6%BC%AC%E3%81%91" class="sns"><i class="fa-brands fa-square-youtube" width="18" height="19"></i></a>
 <a href = "https://www.nicovideo.jp/user/139548104?ref=thumb_nicopedia&transit_from=blogparts_user"><img src = "nico_icon.png" width="14" height="15" class="nico"></a>
 </div><br><p style="text-align:center">©Probably around 2026. lunareclipse.onrender.com Unauthorized reproduction is permitted.</p></div>
 
