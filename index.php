fc830fa1c6c969321207b28709a08244c910447a
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-09-10 10:15:40 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -91,8 +91,8 @@ echo $alert3;
   $comment = $_POST['comment'];
   $log = $comment . $name . "\n";
   $time = date('Y-m-d H:i:s');
-  $post = 'TEST<div style="display: flex; align-items: baseline; gap: 10px;">TEST<a href="' . $title . '">TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<p class="hai">' . $name . 'TEST</p>TEST</div>TEST<hr style="border: 2px solid #A9B2C3;">' . "\n";
- $post = $post . "TEST<br>" . 'TEST<i class="fa-solid fa-thumbs-up">TEST</i>' . 'TEST<i class="fa-solid fa-download">TEST</i>' . 'TEST<i class="fa-solid fa-arrow-up-from-bracket">TEST</i>';
+  $post = 'TEST<div style="display: flex; align-items: baseline; gap: 10px;">TEST<a href="' . $title . '">TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<p class="hai">' . $name . 'TEST</p>TEST</div>' . "\n" . 'TEST<i class="fa-solid fa-thumbs-up">TEST</i>TEST<i class="fa-solid fa-download">TEST</i>TEST<i class="fa-solid fa-arrow-up-from-bracket">TEST</i>TEST<hr style="border: 2px solid #A9B2C3;">' . "\n";
+. ;
   if (!file_exists('posts.txt')) {
   file_put_contents('posts.txt', pack("C*", 0xEF, 0xBB, 0xBF));
   }
