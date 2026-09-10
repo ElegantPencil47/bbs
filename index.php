ecb1354e218b640e83665e1316e64c42cdb8fe8b
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-09-10 11:39:58 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ echo $alert3;
   $comment = $_POST['comment'];
   $log = $comment . $name . "\n";
   $time = date('Y-m-d H:i:s');
-  $post = 'TEST<div style="display: flex; align-items: baseline; gap: 10px;">TEST<a href="' . $title . '">TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<p class="hai">' . $name . $ip. 'TEST<i class="fa-solid fa-thumbs-up">TEST</i>TEST<p> TEST</p>TEST<i class="fa-solid fa-download">TEST</i>TEST<p> TEST</p>TEST<i class="fa-solid fa-arrow-up-from-bracket">TEST</i>TEST<p> TEST</p>TEST<hr style="border: 2px solid #A9B2C3;">' . "\n";
+  $post = 'TEST<a href="' . $title . '">TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<p class="hai">' . $name . $ip. 'TEST<i class="fa-solid fa-thumbs-up">TEST</i>TEST<p> TEST</p>TEST<i class="fa-solid fa-download">TEST</i>TEST<p> TEST</p>TEST<i class="fa-solid fa-arrow-up-from-bracket">TEST</i>TEST<p> TEST</p>TEST<hr style="border: 2px solid #A9B2C3;">' . "\n";
   if (!file_exists('posts.txt')) {
   file_put_contents('posts.txt', pack("C*", 0xEF, 0xBB, 0xBF));
   }
