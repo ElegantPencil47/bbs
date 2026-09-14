2203c0ad926449e9b2c259d65609791c49b6daf9
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-09-14 14:17:05 +0900

    Update index.php

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ header('Location: ' . $_SERVER['REQUEST_URI']);
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.0.10/font-awesome-animation.css" type="text/css" media="all" />
 <meta name="google-site-verification" content="E1-qGHs4Av0Gu3_r49O5U0dyXC-FKoR5FCZuzXtwx24" />
-
+<meta name="description" content="小規模SNS　LunarEclipse(ルナエクリプス)、学問の議論からアングラな話題まで。誰得の月齢確認機能も搭載"/>
 
   <!-- *** google reCAPTHA *** -->
   <script>
