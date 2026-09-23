d36b38953dedd4651902f9fc9d0722662749f47b
ElegantPencil47 <kazuki.komi538@gmail.com>
2026-09-24 08:58:20 +0900

    Update index.php

--- index.php
+++ index.php
@@ -98,12 +98,13 @@ echo $alert3;
   $log = $comment . $name . "\n";
   $time = date('Y-m-d H:i:s');
   $post = 'TEST<div class="user">TEST<a href="' . $title . '">TEST<h2>' . $comment . 'TEST</h2>TEST</a>TEST<p class="hai">' . $name .'TEST</p>TEST<p class="neon">' . $ip . 'TEST</div>' . "\n" . 'TEST<i class="fa-solid fa-thumbs-up">TEST</i>TEST<i class="fa-solid fa-download">TEST</i>TEST<i class="fa-solid fa-arrow-up-from-bracket">TEST</i>TEST</p>TEST<hr style="border: 2px solid #A9B2C3;">' . "\n";
-		  if (is_uploaded_file($_FILES['image']['tmp_name'])) {
+if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+  if (is_uploaded_file($_FILES['image']['tmp_name'])) {
     $image_path = 'images/' . $_FILES['image']['name'];
     move_uploaded_file($_FILES['image']['tmp_name'], $image_path);
 	if (!$image_path == ''){
     $post = 'TEST<div class="user">TEST<a href="' . $title . '">TEST<h2>' . $comment . ' <img src="' . $image_path . '">' . 'TEST</h2>TEST</a>TEST<p class="hai">' . $name .'TEST</p>TEST<p class="neon">' . $ip . 'TEST</div>' . "\n" . 'TEST<i class="fa-solid fa-thumbs-up">TEST</i>TEST<i class="fa-solid fa-download">TEST</i>TEST<i class="fa-solid fa-arrow-up-from-bracket">TEST</i>TEST</p>TEST<hr style="border: 2px solid #A9B2C3;">' . "\n";
-  }}
+  }}}
   if (!file_exists('posts.txt')) {
   file_put_contents('posts.txt', pack("C*", 0xEF, 0xBB, 0xBF));
   }
